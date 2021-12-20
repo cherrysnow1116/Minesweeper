a06b326f0a8b58cbe5028b52ddd6310bdafc6d10
cherrysnow1116 <s1F102102526@iniad.org>
2021-12-20 20:23:11 +0900

    爆弾製作

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import { useState } from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
@@ -12,7 +13,7 @@ const Board = styled.div`
   background-color: white;
   margin: 0;
   position: absolute;
-  top: 10%;
+  top: 50%;
   left: 50%;
   margin-right: -50%;
   transform: translate(-50%, -50%);
@@ -22,14 +23,83 @@ const Face = styled.div`
   height: 30px;
   border-radius: 50%;
   background: yellow;
-  transform: translate(400%, 425%);
+  transform: translate(400%, 135%);
 `
-const Basis = styled.div``
+const Block = styled.div<{ isOpen: boolean; num: number }>`
+  display: inline-block;
+  width: 25px;
+  height: 25px;
+  font-weight: bold;
+  vertical-align: bottom;
+  text-align: center;
+  background-color: ${(props) => (props.isOpen ? 'white' : `grey`)};
+  border: 1px solid black;
+  color: ${(props) => (props.num < 9 && props.num > 0 ? COLORS[props.num - 1] : 'black')};
+  border-bottom: transparent; /* 下が透明になる */
+`
+const Field = styled.div`
+  position: absolute;
+  top: 50px;
+  right: 0;
+  bottom: 10px;
+  left: 0;
+  width: 225px;
+  height: 225px;
+  margin: auto;
+  background-color: #d6cdcd;
+`
+const BombBlock = styled.div`
+  background-color: #faf4f4;
+  color: red;
+  display: inline-block;
+  width: 25px;
+  height: 25px;
+  font-weight: bold;
+  vertical-align: bottom;
+  text-align: center;
+  border: 1px solid black;
+  border-bottom: transparent;
+`
+const COLORS = ['blue', 'green', 'red', 'purple', 'brown', 'yellow', 'black', 'pink']
 const Home: NextPage = () => {
+  const [bombs, setbombs] = useState([{ x: 0, y: 0 }])
+  // prettier-ignore
+  const [board,setBoard]=useState([
+  [9, 9, 9, 9, 9, 9, 9, 9, 9],
+  [9, 9, 9, 9, 9, 9, 9, 9, 9],
+  [9, 9, 9, 9, 9, 9, 9, 9, 9],
+  [9, 9, 9, 9, 9, 9, 9, 9, 9],
+  [9, 9, 9, 9, 9, 9, 9, 9, 9],
+  [9, 9, 9, 9, 9, 9, 9, 9, 9],
+  [9, 9, 9, 9, 9, 9, 9, 9, 9],
+  [9, 9, 9, 9, 9, 9, 9, 9, 9],
+  [9, 9, 9, 9, 9, 9, 9, 9, 9],
+  
+  ])
+  const onClick = (x: number, y: number) => {
+    console.log(x, y)
+    const newBoard: number[][] = JSON.parse(JSON.stringify(board))
+    newBoard[y][x] = bombs.some((bomb) => bomb.x === x && bomb.y === y) ? 10 : 1
+    setBoard(newBoard)
+  }
+
   return (
     <Container>
       <Board>
         <Face></Face>
+        <Field>
+          {board.map((row, y) =>
+            row.map((num, x) =>
+              num === 10 ? (
+                <BombBlock>●</BombBlock>
+              ) : (
+                <Block key={`${x}-${y}`} isOpen={num < 9} num={num} onClick={() => onClick(x, y)}>
+                  {num < 9 && num !== 0 && num}
+                </Block>
+              )
+            )
+          )}
+        </Field>
       </Board>
     </Container>
   )
